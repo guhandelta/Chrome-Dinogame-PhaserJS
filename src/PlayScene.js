82dbd5cd4f2b3c29a924f09ad7fe92ca942bc0ab
Kala Foods <guhan.delta@gmail.com>
2020-11-14 18:50:06 -0500

    fixed the glitch

--- src/PlayScene.js
+++ src/PlayScene.js
@@ -37,7 +37,7 @@ class PlayScene extends Phaser.Scene {
     }) 
 
     this.anims.create({ //Provide a configuration object for the animation
-      key: 'dino-down-aim', // To identify the animation with a key
+      key: 'dino-down-anim', // To identify the animation with a key
       frames: this.anims.generateFrameNumbers('dino-down', { start: 0, end: 1 }), // defines set of frames-
       //- within which the animation exists
       frameRate: 10, //Repeating 10 times per second
@@ -54,12 +54,32 @@ class PlayScene extends Phaser.Scene {
   }
 
   handleInputs(){
-
+    // Dino Jump
     this.input.keyboard.on('keydown_SPACE', ()=>{
       // Don't set any VelocityY, if the dino is not on the floor
       if(!this.dino.body.onFloor()) { return ; }
+
+      this.dino.body.height=92;
+      this.dino.body.offset.y=0; //Offset is reqiured only when kneeling
+
       // How fast should the dino image land back on the ground
       this.dino.setVelocityY(-1600);
+    })
+    // Dino Kneel
+    this.input.keyboard.on('keydown_DOWN', ()=>{
+      // Don't set any VelocityY, if the dino is not on the floor
+      if(!this.dino.body.onFloor()) { return ; }
+      
+      // Change the height of the dino, while kneeling
+      this.dino.body.height=58;
+      this.dino.body.offset.y=34;
+    })
+    // Dino Run
+    this.input.keyboard.on('keyup_DOWN', ()=>{
+      
+      // Change the height of the dino, while kneeling
+      this.dino.body.height=92;
+      this.dino.body.offset.y=0;
     })
   }
 
@@ -74,7 +94,11 @@ class PlayScene extends Phaser.Scene {
       this.dino.anims.stop(); // Stop all the other animations
       this.dino.setTexture('dino'); // This will assign the image where it is not moving
     }else{ // When dino is running
-      this.dino.play('dino-run', true); //true- will ignore the fn call, if it is already running
+      // this.dino.play('dino-run', true); //true- will ignore the fn call, if it is already running
+
+      this.dino.body.height <= 58 ? 
+          this.dino.play('dino-down-anim', true) : 
+          this.dino.play('dino-run', true);
     }
   }
 }
