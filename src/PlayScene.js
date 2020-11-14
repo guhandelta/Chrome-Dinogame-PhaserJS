1e1fa8af2e96436fb56dd02d5635fa3207aa5d34
Kala Foods <guhan.delta@gmail.com>
2020-11-14 14:33:52 -0500

    Provided basic motion animation to the ground, using the update()

--- src/PlayScene.js
+++ src/PlayScene.js
@@ -7,18 +7,23 @@ class PlayScene extends Phaser.Scene {
   }
 
   create() {
+    this.gameSpeed = 10;
     // Receive the Height and Width from this.game.config
     const { height, width } = this.game.config;
 
     // Adding the ground using tileSprite(x, y, width, height, imageFileToDisplay), which- 
     //- can be scrolled infinitely
     this.ground = this.add.tileSprite(0, height, width, 26, 'ground').setOrigin(0, 1);
-    
-    this.dino = this.physics.add.sprite(0, 0, 'dino-idle').setOrigin(0, 1);
+    // An Arcade Physics Sprite is a Sprite with an Arcade Physics body and related components.  
+    //- The body can be dynamic or static | unlike Arcade Image, Arcade sprite may be animated
+    this.dino = this.physics.add.sprite(0, height, 'dino-idle').setOrigin(0, 1);
   }
 
+  // update() is called 60 times/sec => 60FPS
   update() {
 
+    // In every call of the update(), access the ground and update the new location of the image
+    this.ground.tilePositionX += this.gameSpeed;
   }
 }
 
